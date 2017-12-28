9dafee97e7f0ab2cddfa98f8b66f4c27b30c2891
Mario <mariojorge.sdei@gmail.com>
2017-12-28 11:17:38 -0200

    Alterações

--- Aprendendo/Assets/Scripts/Enemy.cs
+++ Aprendendo/Assets/Scripts/Enemy.cs
@@ -4,13 +4,73 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+
+    public float speed;
+    Rigidbody2D rb;
+    bool facingRight = false;
+    bool noChao = false;
+    Transform groundCheck;
+
+    public float jumpForce = 1000;
+
 	// Use this for initialization
 	void Start () {
-		
+
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        groundCheck = transform.Find("EnemyGroundCheck");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        noChao = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        if (!noChao)
+        {
+            speed *= -1;
+        }
+
 	}
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+
+        if (speed > 0 && !facingRight)
+        {
+            Flip();
+        }else if(speed < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+
+            other.gameObject.GetComponent<Enemy>().enabled = false;
+
+            BoxCollider2D[] boxes = gameObject.GetComponents<BoxCollider2D>();
+            foreach(BoxCollider2D box in boxes)
+            {
+                box.enabled = false;
+            }
+
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+
+            speed = 0;
+            transform.Rotate(new Vector3(0, 0, -180));
+            Destroy(gameObject, 3);
+
+        }
+    }
+
 }
